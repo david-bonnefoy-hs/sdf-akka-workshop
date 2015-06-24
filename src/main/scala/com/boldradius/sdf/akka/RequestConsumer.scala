efa67e3f54901818125bf964ba7b8fafb6146957
David Bonnefoy <david.bonnefoy@hootsuite.com>
2015-06-24 11:29:03 -0700

    Card #2

--- src/main/scala/com/boldradius/sdf/akka/RequestConsumer.scala
+++ src/main/scala/com/boldradius/sdf/akka/RequestConsumer.scala
@@ -3,12 +3,19 @@ package com.boldradius.sdf.akka
 import akka.actor.{Props, ActorRef, ActorLogging, Actor}
 import akka.actor.Actor.Receive
 
+import scala.collection.mutable.ListBuffer
+
 /**
  * Created by davidb on 15-06-24.
  */
 class RequestConsumer extends Actor with ActorLogging {
+
+  val receivedRequests: ListBuffer[Request] = new ListBuffer[Request]
+
   override def receive: Receive = {
-    case request: Request => log.info("Consumer received {}", request)
+    case request: Request =>
+      log.info("Consumer received {}", request)
+      receivedRequests += request
   }
 }
 
