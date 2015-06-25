d8db7d544f0a5d1dd3ab3fee79e19206695802d9
jonathann <jonathan.neufeld@hootsuite.com>
2015-06-25 13:57:09 -0700

    Average time per URL statistics

--- src/main/scala/com/boldradius/sdf/akka/StatsAggregatorActor.scala
+++ src/main/scala/com/boldradius/sdf/akka/StatsAggregatorActor.scala
@@ -1,8 +1,11 @@
 package com.boldradius.sdf.akka
 
+import java.time._
+
 import akka.actor.{Actor, Props}
 
 import scala.collection.mutable.ListBuffer
+import scala.concurrent.duration._
 
 /**
  * Created by jonathann on 2015-06-24.
@@ -26,7 +29,68 @@ class StatsAggregatorActor extends Actor
         values =
           receivedRequests.groupBy( _.browser ).mapValues( _.size )
       )
+
+    case GetBusiestMinuteOfDay =>
+      implicit val requestCountOrdering = new Ordering[BusiestMinuteOfDay]
+      {
+        override def compare(x: BusiestMinuteOfDay, y: BusiestMinuteOfDay): Int = x.requests - y.requests
+      }
+
+      val ((hour, minute), requests) =
+        receivedRequests.groupBy {
+          x =>
+            val time = LocalDateTime.ofEpochSecond(x.timestamp, 0, ZoneOffset.UTC)
+
+            (time.getHour, time.getMinute)
+        } mapValues ( _.size ) maxBy (_._2)
+
+      sender() ! BusiestMinuteOfDay(hour, minute, requests)
+
+    case GetURLDistribution =>
+      val denominator = receivedRequests.size.toFloat
+      val result = receivedRequests.groupBy ( _.url ) mapValues ( _.size.toFloat / denominator )
+
+      sender() ! URLDistribution(result)
+
+    case GetAverageURLVisitTime =>
+
+      def pageVisitTimes (requests: List[Request]): List[(String, FiniteDuration)] =
+      {
+        val sliders: Iterator[List[Request]] = requests.sortBy(_.timestamp).sliding(2)
+        val leading =
+          sliders map {
+            case left :: right :: Nil =>
+              (left.url, (right.timestamp - left.timestamp) seconds)
+          } toList
+
+        val maybeInfiniteLast = requests.lastOption map {
+          request =>
+            (request.url, 20 seconds)
+        }
+
+        leading ::: maybeInfiniteLast.toList
+      }
+
+      def averageTimes (completeUrlVisitTimes: List[(String, FiniteDuration)]): Map[String, FiniteDuration] =
+      {
+        def average (values: List[FiniteDuration]): FiniteDuration = {
+          val sum =
+            (values fold (0 seconds)) { (left, right) => left + right }
+
+          sum / values.size
+        }
+
+        completeUrlVisitTimes groupBy ( _._1 ) mapValues {
+          sessionUrlVisitTimes =>
+            average(sessionUrlVisitTimes map (_._2))
+        }
+      }
+
+      sender() ! AverageURLVisitTime(averageTimes(requestsBySession flatMap pageVisitTimes toList))
   }
+
+  def requestsBySession: Traversable[List[Request]] =
+    receivedRequests.toList.groupBy( _.sessionId ).values
 }
 
 object StatsAggregatorActor
@@ -34,9 +98,15 @@ object StatsAggregatorActor
   sealed trait Responses
   sealed trait Statistic extends Responses
   case class RequestsPerBrowser(values: Map[String, Int]) extends Statistic
+  case class BusiestMinuteOfDay(hour: Int, minute: Int, requests: Int) extends Statistic
+  case class URLDistribution(values: Map[String, Float]) extends Statistic
+  case class AverageURLVisitTime(values: Map[String, FiniteDuration]) extends Statistic
 
   sealed trait Requests
   case object GetRequestsPerBrowser extends Requests
+  case object GetBusiestMinuteOfDay extends Requests
+  case object GetURLDistribution extends Requests
+  case object GetAverageURLVisitTime extends Requests
   
   def props() = Props(new StatsAggregatorActor)
 
--- src/test/scala/com/boldradius/sdf/akka/StatsAggregatorActorSpec.scala
+++ src/test/scala/com/boldradius/sdf/akka/StatsAggregatorActorSpec.scala
@@ -1,5 +1,7 @@
 package com.boldradius.sdf.akka
 
+import java.time.{LocalDateTime, ZoneOffset}
+
 import akka.actor.ActorRef
 import akka.pattern.ask
 import akka.testkit.EventFilter
@@ -14,10 +16,14 @@ import scala.concurrent.{Await, Future}
  */
 class StatsAggregatorActorSpec extends BaseAkkaSpec
 {
+  implicit val timeout = Timeout(5 second)
+
+  private def dayPeriod(year: Int, month: Int, day: Int)(hour: Int, minute: Int): Long =
+    LocalDateTime.of(year, month, day, hour, minute).toEpochSecond(ZoneOffset.UTC)
+
+
   "StatsAggregator" should {
     "report browser usage statistics" in {
-
-      implicit val timeout = Timeout(100 day)
 
       val stats: ActorRef = system.actorOf(StatsAggregatorActor.props())
 
@@ -27,6 +33,94 @@ class StatsAggregatorActorSpec extends BaseAkkaSpec
       stats ! Request(1, 0, "lolcats.org", "referer1", "Opera")
       val future: Future[RequestsPerBrowser] = (stats ? GetRequestsPerBrowser).mapTo[RequestsPerBrowser]
       Await.result(future, atMost = 5 second) should be (RequestsPerBrowser(Map("IE" -> 3, "Opera" -> 1)))
+    }
+
+    "report business minute of day" in {
+      val stats: ActorRef = system.actorOf(StatsAggregatorActor.props())
+
+      val time = dayPeriod(2015, 1, 14) _
+
+      stats ! Request(1, time(12, 5), "url", "referer", "browser")
+      stats ! Request(1, time(11, 5), "url", "referer", "browser")
+      stats ! Request(1, time(12, 6), "url", "referer", "browser")
+      stats ! Request(1, time(12, 5), "url", "referer", "browser")
+      stats ! Request(1, time(10, 7), "url", "referer", "browser")
+
+      val future: Future[BusiestMinuteOfDay] = (stats ? GetBusiestMinuteOfDay).mapTo[BusiestMinuteOfDay]
+      Await.result(future, atMost = 5 second) should be (BusiestMinuteOfDay(hour = 12, minute = 5, requests = 2))
+    }
+
+    "report URL distribution" in {
+      val stats: ActorRef = system.actorOf(StatsAggregatorActor.props())
+
+      stats ! Request(1, 0, "google.com", "referer", "browser")
+      stats ! Request(1, 0, "microsoft.com", "referer", "browser")
+      stats ! Request(1, 0, "boldradius.com", "referer", "browser")
+      stats ! Request(2, 0, "hootsuite.com", "referer", "browser")
+      stats ! Request(2, 0, "google.com", "referer", "browser")
+      stats ! Request(2, 0, "google.com", "referer", "browser")
+      stats ! Request(3, 0, "microsoft.com", "referer", "browser")
+      stats ! Request(3, 0, "google.com", "referer", "browser")
+      stats ! Request(3, 0, "hootsuite.com", "referer", "browser")
+      stats ! Request(4, 0, "google.com", "referer", "browser")
+
+      val future: Future[URLDistribution] = (stats ? GetURLDistribution).mapTo[URLDistribution]
+      Await.result(future, atMost = 5 second) should be (
+        URLDistribution(
+          Map(
+            "google.com" -> 0.5f,
+            "microsoft.com" -> 0.2f,
+            "hootsuite.com" -> 0.2f,
+            "boldradius.com" -> 0.1f
+          )
+        )
+      )
+    }
+
+    "report average page visit times" in {
+      val time = dayPeriod(2015, 1, 14) _
+      val stats: ActorRef = system.actorOf(StatsAggregatorActor.props())
+
+      stats ! Request(1, time(1, 5), "google.com", "referer", "browser")
+      stats ! Request(1, time(1, 15), "microsoft.com", "referer", "browser")
+      stats ! Request(1, time(4, 12), "microsoft.com", "referer", "browser")
+      stats ! Request(1, time(8, 15), "boldradius.com", "referer", "browser")
+      stats ! Request(1, time(8, 16), "boldradius.com", "referer", "browser")
+      stats ! Request(1, time(8, 20), "boldradius.com", "referer", "browser")
+      stats ! Request(1, time(8, 24), "google.com", "referer", "browser")
+      stats ! Request(1, time(8, 25), "boldradius.com", "referer", "browser")
+      stats ! Request(1, time(8, 29), "google.com", "referer", "browser")
+      stats ! Request(1, time(8, 32), "microsoft.com", "referer", "browser")
+      stats ! Request(1, time(11, 40), "hootsuite.com", "referer", "browser")
+      stats ! Request(1, time(11, 45), "hootsuite.com", "referer", "browser")
+      stats ! Request(1, time(11, 55), "microsoft.com", "referer", "browser")
+      stats ! Request(1, time(12, 0), "google.com", "referer", "browser")
+      stats ! Request(2, time(1, 0), "google.com", "referer", "browser")
+      stats ! Request(2, time(1, 17), "microsoft.com", "referer", "browser")
+      stats ! Request(2, time(4, 11), "microsoft.com", "referer", "browser")
+      stats ! Request(2, time(8, 16), "boldradius.com", "referer", "browser")
+      stats ! Request(2, time(8, 18), "boldradius.com", "referer", "browser")
+      stats ! Request(2, time(8, 22), "boldradius.com", "referer", "browser")
+      stats ! Request(2, time(8, 25), "google.com", "referer", "browser")
+      stats ! Request(2, time(8, 26), "boldradius.com", "referer", "browser")
+      stats ! Request(2, time(8, 34), "google.com", "referer", "browser")
+      stats ! Request(2, time(8, 48), "microsoft.com", "referer", "browser")
+      stats ! Request(2, time(11, 40), "hootsuite.com", "referer", "browser")
+      stats ! Request(2, time(11, 45), "hootsuite.com", "referer", "browser")
+      stats ! Request(2, time(11, 55), "microsoft.com", "referer", "browser")
+      stats ! Request(2, time(12, 0), "google.com", "referer", "browser")
+
+      val future: Future[AverageURLVisitTime] = (stats ? GetAverageURLVisitTime).mapTo[AverageURLVisitTime]
+      Await.result(future, atMost = 5 second) should be (
+        AverageURLVisitTime(
+          Map(
+            "google.com" -> (5.minutes + 50.seconds),
+            "microsoft.com" -> (2.hours + 31.minutes + 7.seconds + 500.millis),
+            "hootsuite.com" -> (7.minutes + 30.seconds),
+            "boldradius.com" -> (3.minutes + 45.seconds)
+          )
+        )
+      )
     }
   }
 
